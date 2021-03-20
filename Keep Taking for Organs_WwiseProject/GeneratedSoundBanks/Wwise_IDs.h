3a3c84378c2e35a1cb31b80399fb457469709cfa
Karen <a8588611@gmail.com>
2021-03-20 20:05:10 +0800

    ICU

---
+++ Keep Taking for Organs_WwiseProject/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,96 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_ELE_TRAP = 1376857212U;
+        static const AkUniqueID PLAY_END_HIDING = 2938009645U;
+        static const AkUniqueID PLAY_ENTERDIGIT = 2865869223U;
+        static const AkUniqueID PLAY_ESCLEAVE = 702431094U;
+        static const AkUniqueID PLAY_ESCMENU = 1762940376U;
+        static const AkUniqueID PLAY_GET_ORGAN = 2614804272U;
+        static const AkUniqueID PLAY_LEAVEMENU = 2784792558U;
+        static const AkUniqueID PLAY_MUSIC = 2932040671U;
+        static const AkUniqueID PLAY_PLAYER_DEATH = 1835085974U;
+        static const AkUniqueID PLAY_PLAYER_FOOTSTEP = 1724675634U;
+        static const AkUniqueID PLAY_PLAYER_GUNSHOT = 33727574U;
+        static const AkUniqueID PLAY_PLAYER_SABER = 3877648163U;
+        static const AkUniqueID PLAY_PLAYERASTART = 4238984918U;
+        static const AkUniqueID PLAY_PLAYERBSTART = 1003576063U;
+        static const AkUniqueID PLAY_PRESSENTER = 392107407U;
+        static const AkUniqueID PLAY_ROBOT_DEATH = 2426976433U;
+        static const AkUniqueID PLAY_ROBOT_ELEGUN = 3233088915U;
+        static const AkUniqueID PLAY_ROBOT_LASERGUN = 2371690888U;
+        static const AkUniqueID PLAY_ROBOT_MOVE = 841165746U;
+        static const AkUniqueID PLAY_SELECTSTAGE = 2964532456U;
+        static const AkUniqueID PLAY_START = 2230670290U;
+        static const AkUniqueID PLAY_START_HIDING = 882926292U;
+        static const AkUniqueID PLAY_TAB = 2419250855U;
+        static const AkUniqueID STOP_ALLSFX = 1217524018U;
+        static const AkUniqueID STOP_PLAYER_FOOTSTEP = 2005445724U;
+        static const AkUniqueID STOP_ROBOT_MOVE = 2672046660U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace GAME
+        {
+            static const AkUniqueID GROUP = 702482391U;
+
+            namespace STATE
+            {
+                static const AkUniqueID NONE = 748895195U;
+            } // namespace STATE
+        } // namespace GAME
+
+        namespace MUSIC_STAGE
+        {
+            static const AkUniqueID GROUP = 4044678609U;
+
+            namespace STATE
+            {
+                static const AkUniqueID NONE = 748895195U;
+            } // namespace STATE
+        } // namespace MUSIC_STAGE
+
+    } // namespace STATES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID MASTER_VOLUMN = 4179668891U;
+        static const AkUniqueID MUSIC_VOLUMN = 1006694112U;
+        static const AkUniqueID SFX_VOLUMN = 1564184904U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID SOUNDBANK = 1661994096U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID SFX = 393239870U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
